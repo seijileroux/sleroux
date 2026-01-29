80c28c27c7742942949a192feaf3db1993b47091
Seiji Leroux <45913147+seijileroux@users.noreply.github.com>
2026-01-29 22:23:14 +0300

    Update bio

--- app/data/data.ts
+++ app/data/data.ts
@@ -1,7 +1,7 @@
 export const personalInfo = {
   name: "Seiji",
   title: "front_end_dev, linux_enthusiast",
-  bio: "i write code that (mostly) works. currently vibing with typescript, next.js, tailwind, and way too much coffee ☕",
+  bio: "i write code that (mostly) works. currently working with typescript, next.js, tailwind, and way too much coffee ☕",
   email: "s.leroux.dev@proton.me",
   location: "~/internet",
 };
